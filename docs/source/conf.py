c5933d97252db3c121859d513205f55b675f6caa
Dmitry Frolov <dmitsf@gmail.com>
2020-01-10 13:51:43 +0300

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+print(sys.path)
 
 
 # -- General configuration ------------------------------------------------
@@ -31,10 +33,6 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.doctest',
